a158a6cde071777b74f0a4374504eb3cbb8fa0e3
Sourav Sanjay <souravsanjay07@gmail.com>
2026-02-24 01:15:36 +0530

    Created embeddings for an array of words

--- 02-Gemini_Embedding/index.js
+++ 02-Gemini_Embedding/index.js
@@ -1,13 +1,34 @@
 import dotenv from "dotenv/config"
 import { GoogleGenAI } from "@google/genai"
+import {readFileSync, writeFileSync} from "fs"
 
-const googleClient = new GoogleGenAI({
-    apiKey: process.env.GEMINI_KEY
-})
+async function readJsonFile(){
+    const data = readFileSync("data.json");
+    const stringData = JSON.parse(data.toString())
 
-const response = await googleClient.models.embedContent({
-    model: 'gemini-embedding-001',
-    contents: "This is my life"
-})
+    let responseData = await createDimensions(stringData);
+    responseData = responseData.map((item, index)=>{
+        return {input: stringData[index], embedding: item}
+    })
+    createJsonFile(responseData, "embedding.json");
+}
 
-console.log(response.embeddings);
+async function createDimensions(data){
+    const googleClient = new GoogleGenAI({
+        apiKey: process.env.GEMINI_KEY
+    })
+
+    let response = await googleClient.models.embedContent({
+        model: 'gemini-embedding-001',
+        contents: data
+    })
+
+    return response.embeddings;
+}
+
+function createJsonFile(data, file){
+    const fileData = JSON.stringify(data);
+    writeFileSync(file, fileData);
+}
+
+readJsonFile();

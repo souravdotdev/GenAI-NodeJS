ace4c42dd7169b0307e400a729dc1e2b8f4ada47
Sourav Sanjay <souravsanjay07@gmail.com>
2026-02-27 23:40:56 +0530

    Implemented both batch and stream methods.

--- 03-LangChain/index.js
+++ 03-LangChain/index.js
@@ -8,9 +8,26 @@ const client = new ChatGoogle({
 
 
 async function generateResponse(){
-    const response = await client.invoke("Tell me 5 good reasons to visit Ottapalam in Kerala");
+    // const response = await client.invoke("Tell me 5 good reasons to visit Ottapalam in Kerala");
 
-    console.log(response.content);
+    // console.log(response.content);
+
+    // const response = await client.batch([
+    //     "Tell me 5 animals startitng with A",
+    //     "Tell me 5 football players starting with C"
+    // ])
+
+    // for(let i = 0; i < response.length; i++){
+    //     console.log(response[i].content);
+    // }
+
+
+    const response = await client.stream("Tell me 5 reasons to visit Ottapalam in Kerala");
+
+    for await (let chunks of response){
+        console.log(chunks.content);
+    }
+
 }
 
 generateResponse();
